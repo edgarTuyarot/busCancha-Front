aa328eb03ffc345b2ae40ce3f08a48b21e81e2ee
Loulego <edgar130289@gmail.com>
2021-12-15 15:22:07 -0300

    Merge branch 'master' of https://github.com/edgarTuyarot/proyecto.Frik

---
+++ src/components/TablaReservas.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container-sm">
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+           <th scope="col">Usuario</th>
+          <th scope="col">Fecha</th>
+          <th scope="col">Hora</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(reserva, index) in reservas" :key="index">
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ reserva.usuario_id }}</td>
+          <td>{{ reserva.fecha }}</td>
+          <td>{{ reserva.hora }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+    props:{
+        reservas:[]
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Propietario.vue
+++ src/views/Propietario.vue
@@ -5,55 +5,43 @@
 
   <Navbar />
 
-  <Calendario />
-
- <div class="container-sm">
-    <table class="table table-striped table-hover">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-           <th scope="col">Usuario</th>
-          <th scope="col">Fecha</th>
-          <th scope="col">Hora</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(reserva, index) in reservasProp" :key="index">
-          <th scope="row">{{ index + 1 }}</th>
-          <td>{{ reserva.usuario_id }}</td>
-          <td>{{ reserva.fecha }}</td>
-          <td>{{ reserva.hora }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-
- 
+  <h2>Reservas del Mes</h2>
+  <TablaReservas :reservas="reservasMes" />
 </template>
 
 <script>
-import { provide } from "@vue/runtime-core";
-import { ref } from "@vue/reactivity";
 import Navbar from "../components/Navbar.vue";
-import Calendario from "../components/Calendario.vue";
+import TablaReservas from "../components/TablaReservas.vue";
 import { mapState, mapActions } from "vuex";
+
 export default {
   data() {
-    const reservas = ref([]);
-    provide("reservas", reservas);
     return {
-      reservas,
       propCancha: "",
-      xfechas: [],
-      mes: "",
+      
     };
   },
   components: {
-    Navbar,Calendario
+    Navbar,
+    TablaReservas,
   },
 
   computed: {
     ...mapState(["propietario", "canchas", "reservasProp"]),
+    reservasMes() {
+      let date = new Date();
+      let arrayMes=[]
+      for (const reserva of this.reservasProp) {
+        let fechaReserva=new Date(reserva.fecha);
+        if(date.getMonth()==fechaReserva.getMonth()){
+          arrayMes.push(reserva)
+        }
+        
+      }
+      console.log(date.getMonth());
+
+      return arrayMes;
+    },
   },
   methods: {
     ...mapActions(["findReservasProp"]),
@@ -61,15 +49,8 @@ export default {
       for (const cancha of this.canchas) {
         if (this.propietario.cancha == cancha.id) {
           this.propCancha = cancha.nombre;
-         
         }
-        this.reservas=this.reservasProp;
       }
-    },
-    porFecha() {
-     
-
- 
     },
   },
   created() {
